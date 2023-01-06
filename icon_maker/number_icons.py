9cdc1c3fc584e9df777ed73499b22e95d2cd1da5
mschulist2@gmail.com <mschulist2@gmail.com>
2023-01-05 21:21:14 -0800

    fixed numbers on icons
    Former-commit-id: d976153ba3a50f0e2a677f5686e5155150d8f375

--- icon_maker/number_icons.py
+++ icon_maker/number_icons.py
@@ -6,14 +6,14 @@ n = 100 # number of colors and icons to generate
 def add_number(number):
     '''Add the number to the center of an icon and save it to a new image
     Useful for generating multiple icons'''
-    image = Image.new('RGBA', (100,100), (255,0,0))
+    image = Image.new('RGBA', (60,100), (255,0,0))
     font=ImageFont.truetype('/usr/share/fonts/truetype/ubuntu/Ubuntu-M.ttf', 100)
     draw=ImageDraw.Draw(image)
     x=(image.size[0]-draw.textsize(number,font)[0])/2
     y=(image.size[1]-draw.textsize(number,font)[1])/2
     draw.text((x,y),number,font=font,fill='#FFFFFF')
     #image.show()
-    image.save("./icons/icon_" + str(int(number)-1) + ".png")
+    image.save("./icons/icon_" + number + ".png")
  
-for i in range(1, n + 1):
+for i in range(0, n):
     add_number(str(i))
