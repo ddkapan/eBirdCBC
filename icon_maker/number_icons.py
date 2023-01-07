72012555d5ef31cfaa198d333d48699db87891a0
mschulist2@gmail.com <mschulist2@gmail.com>
2023-01-06 20:23:47 -0800

    made wayyy more icons
    Former-commit-id: e27170a69a268f8d8fbeb95ab93fa6b1bca19138

--- icon_maker/number_icons.py
+++ icon_maker/number_icons.py
@@ -2,15 +2,15 @@ from PIL import Image, ImageFont, ImageDraw
 from distinctipy import distinctipy
 from ast import literal_eval as eval
 
-n = 100 # number of colors and icons to generate
+n = 1000 # number of colors and icons to generate
 # colors = eval(distinctipy.get_colors(n))
 
 
 def add_number(number):
     '''Add the number to the center of an icon and save it to a new image
     Useful for generating multiple icons'''
-    image = Image.new('RGBA', (60,100), (0,0,0,0))
-    font=ImageFont.truetype('/usr/share/fonts/truetype/ubuntu/Ubuntu-M.ttf', 50)
+    image = Image.new('RGBA', (150,75), (0,0,0,0))
+    font=ImageFont.truetype('/usr/share/fonts/truetype/ubuntu/Ubuntu-M.ttf', 75)
     draw=ImageDraw.Draw(image)
     x=(image.size[0]-draw.textsize(number,font)[0])/2
     y=(image.size[1]-draw.textsize(number,font)[1])/2
